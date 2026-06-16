4eb7c9412d8416320788a62444509a2474a69f9a
anwar9136 <191209710+anwar9136@users.noreply.github.com>
2026-06-16 20:27:02 +0530

    Configure API URL using environment variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // 1. Create an Axios Instance
 // This centralizes our configuration (base URL, credentials, etc.)
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true, // Crucial for sending/receiving JWT cookies
 });
 
